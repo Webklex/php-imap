6f8c3a5c3c90fd798f272613890fcf8d31d59081
Menno <menno.vanhout@universal-games.nl>
2021-01-09 16:58:23 +0100

    Fix regex to be case insensetive

--- src/Structure.php
+++ src/Structure.php
@@ -100,7 +100,7 @@ class Structure {
      * Determine the message content type
      */
     public function getBoundary(){
-        $boundary = $this->header->find("/boundary=\"?([^\"]*)[\";\s]/");
+        $boundary = $this->header->find("/boundary=\"?([^\"]*)[\";\s]/i");
 
         if ($boundary === null) {
             return null;
