d2033e7e2ed842880a9e29820adf2e3671e88d61
Michał Kleszczyński <Magiczne@users.noreply.github.com>
2021-09-04 11:39:14 +0200

    Added methods for moving and copying multiple messages (#160)
    * Added moveManyMessages method
    
    * Added copyManyMessages method
    
    * Updated docblocks to better explain return type

--- src/Connection/Protocols/ImapProtocol.php
+++ src/Connection/Protocols/ImapProtocol.php
@@ -825,6 +825,25 @@ class ImapProtocol extends Protocol {
     }
 
     /**
+     * Copy multiple messages to the target folder
+     *
+     * @param array<string> $messages List of message identifiers
+     * @param string $folder Destination folder
+     * @param bool $uid Set to true if you pass message unique identifiers instead of numbers
+     * @return array|bool Tokens if operation successful, false if an error occurred
+     *
+     * @throws RuntimeException
+     */
+    public function copyManyMessages($messages, $folder, $uid = false) {
+        $command = $uid ? 'UID COPY' : 'COPY';
+
+        $set = implode(',', $messages);
+        $tokens = [$set, $this->escapeString($folder)];
+
+        return $this->requestAndResponse($command, $tokens, true);
+    }
+
+    /**
      * Move a message set from current folder to an other folder
      * @param string $folder destination folder
      * @param $from
@@ -843,6 +862,25 @@ class ImapProtocol extends Protocol {
         $command = ($uid ? "UID " : "")."MOVE";
 
         return $this->requestAndResponse($command, [$set, $this->escapeString($folder)], true);
+    }
+
+    /**
+     * Move multiple messages to the target folder
+     *
+     * @param array<string> $messages List of message identifiers
+     * @param string $folder Destination folder
+     * @param bool $uid Set to true if you pass message unique identifiers instead of numbers
+     * @return array|bool Tokens if operation successful, false if an error occurred
+     *
+     * @throws RuntimeException
+     */
+    public function moveManyMessages($messages, $folder, $uid = false) {
+        $command = $uid ? 'UID MOVE' : 'MOVE';
+
+        $set = implode(',', $messages);
+        $tokens = [$set, $this->escapeString($folder)];
+
+        return $this->requestAndResponse($command, $tokens, true);
     }
 
     /**
