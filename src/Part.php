0eca11dec947812ffeea71e01839016aac22ba61
Nourisson Laurent <l.nourisson.pro@gmail.com>
2022-01-28 11:31:09 +0100

    Fix isAttachment that did not properly take in consideration dispositions options

--- src/Part.php
+++ src/Part.php
@@ -301,7 +301,7 @@ class Part {
     public function isAttachment(){
         $valid_disposition = in_array(strtolower($this->disposition), ClientManager::get('options.dispositions'));
 
-        if ($this->type == IMAP::MESSAGE_TYPE_TEXT && ($this->ifdisposition == 0 || (empty($this->disposition))) && !$valid_disposition) {
+        if ($this->type == IMAP::MESSAGE_TYPE_TEXT && ($this->ifdisposition == 0 || empty($this->disposition) || !$valid_disposition)) {
             if (($this->subtype == null || in_array((strtolower($this->subtype)), ["plain", "html"])) && $this->filename == null && $this->name == null) {
                 return false;
             }
