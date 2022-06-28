a206086be6fe648ea8c8f8930c0b80efc3850e03
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2022-06-28 08:17:59 +0200

    Install Rector
    "Rector instantly upgrades and refactors the PHP code of your application." see https://github.com/rectorphp/rector
    
    Using rector will enable less manual work while upgrading.

---
+++ rector.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector;
+use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
+use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Privatization\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
+use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
+use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
+
+return static function (RectorConfig $containerConfigurator): void {
+    $containerConfigurator->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+};
