1c533ae4dd722ca4a16973e641e2120455bc48b5
webklex <info@webklex.com>
2023-01-10 10:52:05 +0100

    Unused ClientManager instance removed

--- tests/issues/Issue355Test.php
+++ tests/issues/Issue355Test.php
@@ -13,19 +13,9 @@
 namespace Tests\issues;
 
 use PHPUnit\Framework\TestCase;
-use Webklex\PHPIMAP\ClientManager;
 use Webklex\PHPIMAP\Header;
 
 class Issue355Test extends TestCase {
-
-    /**
-     * Setup the test environment.
-     *
-     * @return void
-     */
-    public function setUp(): void {
-        $this->cm = new ClientManager();
-    }
 
     public function testIssue() {
         $raw_header = "Subject: =?UTF-8?Q?Re=3A_Uppdaterat_=C3=A4rende_=28447899=29=2C_kostnader_f=C3=B6r_hj=C3=A4?= =?UTF-8?Q?lp_med_stadge=C3=A4ndring_enligt_ny_lagstiftning?=\r\n";
