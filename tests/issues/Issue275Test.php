5cfd7aeaf7366641f4299ee13056e5acdf3ef3d7
webklex <info@webklex.com>
2023-01-18 19:21:03 +0100

    Additional test for issue #275 added

--- tests/issues/Issue275Test.php
+++ tests/issues/Issue275Test.php
@@ -17,12 +17,22 @@ use Webklex\PHPIMAP\Message;
 
 class Issue275Test extends TestCase {
 
-    public function testIssue() {
+    public function testIssueEmail1() {
         $filename = implode(DIRECTORY_SEPARATOR, [__DIR__, "..", "messages", "issue-275.eml"]);
         $message = Message::fromFile($filename);
 
         self::assertSame("Testing 123", (string)$message->subject);
         self::assertSame("testing123 this is a body", $message->getTextBody());
+    }
+
+    public function testIssueEmail2() {
+        $filename = implode(DIRECTORY_SEPARATOR, [__DIR__, "..", "messages", "issue-275-2.eml"]);
+        $message = Message::fromFile($filename);
+
+        $body = "Test\r\n\r\nMed venlig hilsen\r\nMartin Larsen\r\nFeline Holidays A/S\r\nTlf 78 77 04 12";
+
+        self::assertSame("Test 1017", (string)$message->subject);
+        self::assertSame($body, $message->getTextBody());
     }
 
 }
